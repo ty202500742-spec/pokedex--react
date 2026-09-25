172c0aa40c33bab94195cd439538f56d757583b1
dolf <ty202500742@wmsu.edu.ph>
2026-09-26 00:19:24 +0800

    added moire details in the desgin, matched the border colors withthe color of the pokemon, added border design with the descipition, types, abilites, and also imporved the statbar

--- src/App.jsx
+++ src/App.jsx
@@ -14,24 +14,39 @@ function App() {
   const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`
 
 
-  const loadMore = useCallback(() => {
+  const loadMore = useCallback(async () => {
     if (loading || !hasMore) return
     setLoading(true)
-    axios.get(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`).then((response) => {
-      const newItems = response.data.results.map((p) => {
-        const splitUrl = p.url.split('/')
-        const id = splitUrl[splitUrl.length - 2]
-        return { name: p.name, id: id }
 
-      })
+    try {
+      const response = await axios.get(
+        `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`
+      )
+
+      const newItems = await Promise.all(
+        response.data.results.map(async (p) => {
+          const splitUrl = p.url.split('/')
+          const id = splitUrl[splitUrl.length - 2]
+
+          const pokemonResponse = await axios.get(p.url)
+
+          return {
+            name: p.name,
+            id: id,
+            types: pokemonResponse.data.types.map(t => t.type.name)
+          }
+        })
+      )
 
       setPokemonList((prev) => [...prev, ...newItems])
       setOffset((prev) => prev + limit)
       setHasMore(response.data.next !== null)
-      setLoading(false)
 
-    })
+    } catch (error) {
+      console.log(error)
+    }
 
+    setLoading(false)
 
   }, [offset, loading, hasMore])
 
@@ -54,21 +69,21 @@ function App() {
 
 
   function openDetails(id) {
-     setDescription('')
+    setDescription('')
     axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`).then((response) => {
-       const pokemonData = response.data
-    setSelected(pokemonData)
+      const pokemonData = response.data
+      setSelected(pokemonData)
 
-    axios.get(pokemonData.species.url).then((speciesResponse) => {
-      const entry = speciesResponse.data.flavor_text_entries.find(
-        (e) => e.language.name === 'en'
-      )
-      setDescription(
-        entry
-          ? entry.flavor_text.replace(/[\n\f]/g, ' ')
-          : 'No description available.'
-      )
-    })
+      axios.get(pokemonData.species.url).then((speciesResponse) => {
+        const entry = speciesResponse.data.flavor_text_entries.find(
+          (e) => e.language.name === 'en'
+        )
+        setDescription(
+          entry
+            ? entry.flavor_text.replace(/[\n\f]/g, ' ')
+            : 'No description available.'
+        )
+      })
     })
   }
 
@@ -92,7 +107,30 @@ function App() {
 
   }
 
+  function getTypeColor(type) {
+    const colors = {
+      normal: '#A8A77A',
+      fire: '#EE8130',
+      water: '#6390F0',
+      electric: '#F7D02C',
+      grass: '#7AC74C',
+      ice: '#96D9D6',
+      fighting: '#C22E28',
+      poison: '#A33EA1',
+      ground: '#E2BF65',
+      flying: '#A98FF3',
+      psychic: '#F95587',
+      bug: '#A6B91A',
+      rock: '#B6A136',
+      ghost: '#735797',
+      dragon: '#6F35FC',
+      dark: '#705746',
+      steel: '#B7B7CE',
+      fairy: '#D685AD'
+    }
 
+    return colors[type] || '#888'
+  }
   return (
     <div>
       <div className='searchBar'>
@@ -104,23 +142,70 @@ function App() {
       <p className='guide'>Click on a card to see more details</p>
       <div className='searchedCard'>
         {value && (
-          <div className='card' onClick={() => openDetails(value.id)}>
-            <img className="picture" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${value?.id}.png`} />
-            <div className='withCard'>
-              <p>{value?.name}</p>
-              <p>ID: {value?.id}</p>
-            </div>
-          </div>
-        )}
+  <div className="pokemonWrapper">
+
+    <div className="card" onClick={() => openDetails(value.id)}>
+
+      <div className="imageContainer">
+        <span className="pokemonId">#{value.id}</span>
+
+        <img
+          className="picture"
+          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${value.id}.png`}
+        />
+      </div>
+
+      <div className="withCard">
+        <p>{value.name}</p>
+
+        <div className="detailTypes">
+          {value.types.map((t) => (
+            <span
+              key={t.type.name}
+              className={`type-${t.type.name}`}
+            >
+              {t.type.name}
+            </span>
+          ))}
+        </div>
+      </div>
+
+    </div>
+
+  </div>
+)}
       </div>
       <div className='cardList'>
         {pokemonList.map((p) => (
-          <div className='card' key={p.id} onClick={() => openDetails(p.id)}>
-            <img className="picture" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${p.id}.png`} />
-            <div className='withCard'>
-              <p>{p.name}</p>
-              <p>ID: {p.id}</p>
+          <div className="pokemonWrapper" key={p.id}>
+
+            <div className="card" onClick={() => openDetails(p.id)}>
+
+              <div className="imageContainer">
+                <span className="pokemonId">#{p.id}</span>
+
+                <img
+                  className="picture"
+                  src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${p.id}.png`}
+                />
+              </div>
+
+              <div className="withCard">
+                <p>{p.name}</p>
+
+                <div className="pokemonTypes">
+                  {p.types.map((type) => (
+                    <span key={type} className={`type-${type}`}>
+                      {type}
+                    </span>
+                  ))}
+                </div>
+              </div>
+
             </div>
+
+
+
           </div>
         ))}
       </div>
@@ -128,26 +213,94 @@ function App() {
 
       {selected && (
         <div className='modalOverlay' onClick={closeDetails} >
-          <div className='modal' onClick={(e) => e.stopPropagation()}>
+          <div
+            className='modal'
+            onClick={(e) => e.stopPropagation()}
+            style={{
+              '--type-color': getTypeColor(selected.types[0].type.name)
+            }}
+          >
             <button className='closeButton' onClick={closeDetails}>×</button>
-            <img className='modalPicture' src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${selected.id}.png`} />
-            <h2>{selected.name}</h2>
-            <p className='description'>{description || 'Loading description...'}</p>
-            <p>ID: {selected.id}</p>
-            <p>Height: {selected.height}</p>
-            <p>Weight: {selected.weight}</p>
-            <p>Types: {selected.types.map(t => t.type.name).join(', ')}</p>
-            <p>Abilities: {selected.abilities.map(a => a.ability.name).join(', ')}</p>
-            <div className='statsList'>
-              {selected.stats.map((s) =>
-                <div key={s.stat.name} className='statRow'>
-                  <span>{s.stat.name}</span>
-                  <span>{s.base_stat}</span>
-
-                </div>
-              )}
-
+            <img
+              className='modalPicture'
+              style={{
+                borderColor: getTypeColor(selected.types[0].type.name)
+              }}
+              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${selected.id}.png`}
+            />            <h2
+              style={{
+                color: getTypeColor(selected.types[0].type.name)
+              }}
+            >
+              {selected.name}
+            </h2>
+            <div className="description">
+              <span className="descriptionTitle">Description: </span>
+              <p>{description || 'Loading description...'}</p>
             </div>
+
+            <div className="infoList">
+              <div className="infoBox">
+                <span>ID</span>
+                <strong>#{selected.id}</strong>
+              </div>
+
+              <div className="infoBox">
+                <span>Height</span>
+                <strong>{selected.height}</strong>
+              </div>
+
+              <div className="infoBox">
+                <span>Weight</span>
+                <strong>{selected.weight}</strong>
+              </div>
+            </div>
+            <div className="detailSection">
+              <span className="sectionTitle">Types</span>
+
+              <div className="detailTypes">
+                {selected.types.map((t) => (
+                  <span
+                    key={t.type.name}
+                    className={`type-${t.type.name}`}
+                  >
+                    {t.type.name}
+                  </span>
+                ))}
+              </div>
+            </div>
+            <div className="detailSection">
+              <span className="sectionTitle">Abilities</span>
+
+              <div className="abilities">
+                {selected.abilities.map((a) => (
+                  <span key={a.ability.name}>
+                    {a.ability.name}
+                  </span>
+                ))}
+              </div>
+            </div>
+            <div className='statsList'>
+  {selected.stats.map((s) => (
+    <div key={s.stat.name} className='statRow'>
+
+      <div className='statTop'>
+        <span>{s.stat.name}</span>
+        <span>{s.base_stat}</span>
+      </div>
+
+      <div className='statBar'>
+        <div
+          className={`statFill stat-${s.stat.name}`}
+          style={{
+            width: `${Math.min(s.base_stat, 100)}%`
+          }}
+        ></div>
+      </div>
+
+    </div>
+  ))}
+</div>
           </div>
         </div>
       )}
